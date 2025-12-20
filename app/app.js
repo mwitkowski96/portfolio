3d51e409bafa2bee0d106e038991cccebb1f5492
Marek Witkowski <marek.witkowski96@icloud.com>
2025-12-20 12:40:04 +0100

    Podstawowy szkielet aplikacji

---
+++ app/app.js
@@ -0,0 +1,35 @@
+// Podstawowe ustawienia aplikacji
+let appData = {
+  header: {
+    name: "Marek Witkowski",
+    title: "web-designer",
+  },
+  about: {
+    photo: "./img/IMG_0826.jpg",
+    about:
+      "Aspirujący programista i konsultant IT, który łączy techniczne zacięcie z marketingowym wyczuciem. Pomagam firmom optymalizować procesy technologiczne oraz tworzę materiały promocyjne, które skutecznie budują wizerunek marki. Moim celem jest dostarczanie rozwiązań, które pozwolą Twojemu biznesowi w pełni rozwinąć skrzydła i zyskać przewagę na cyfrowym rynku.",
+    skills: [
+      { name: "HTML", yearsOfExperience: 5, img: "./IMG/HTML5.png" },
+      { name: "CSS", yearsOfExperience: 5, img: "./IMG/symbol.png" },
+      { name: "JS", yearsOfExperience: 5, img: "./IMG/Javascript.png" },
+      { name: "Git", yearsOfExperience: 1, img: "./IMG/Shape.png" },
+      { name: "Figma", yearsOfExperience: 1, img: "./IMG/Figma.png" },
+      { name: "Chrome", yearsOfExperience: 5, img: "./IMG/GoogleChrome.png" },
+      { name: "VSCode", yearsOfExperience: 5, img: "./IMG/Group.png" },
+    ],
+  },
+  projects: [
+    { id: 1, title: "Calculator", tech: ["HTML", "CSS", "JavaScript"] },
+    {
+      id: 2,
+      title: "Non-govermnetal organization",
+      tech: ["HTML", "CSS", "JavaScript"],
+    },
+    {
+      id: 3,
+      title: "Portfolio app",
+      tech: ["HTML", "CSS", "JavaScript", "Figma", "Github"],
+    },
+  ],
+  messages: [],
+};
